ce07d22e27d464d81e73f15fe46b2bb815cf224c
AniKal25 <kal21006@byui.edu>
2022-09-18 00:08:41 +0100

    Another commit

--- routes/contacts/contacts.js
+++ routes/contacts/contacts.js
@@ -2,6 +2,6 @@ const routes = require('express').Router();
 const {getAllContacts, getContactById} = require('../../controllers/contacts/contacts')
 
 routes.get('/', getAllContacts);
-routes.get('/id/:id', getContactById)
+routes.get('/:id', getContactById)
 
 module.exports = routes;
