dbdee459b3b7e6b554e1f2c3bd31b77ba1a17cca
AniKal25 <kal21006@byui.edu>
2022-09-24 22:58:30 +0100

    Personal Assignment 04

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,15 @@ const express = require('express');
 //const contacts = require('../model/contacts/contacts');
 const routes = require('express').Router();
 const contacts = require('./contacts/contacts');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 
 
 routes.use('/contacts', contacts);
+
+routes.use('/api-docs', swaggerUi.serve);
+routes.get('/api-docs', swaggerUi.setup(swaggerDocument, {explorer: true}));
 
 routes.get('/', (req, res) => {
     res.send('Health check');
--- server.js
+++ server.js
@@ -24,6 +24,15 @@ async function connect() {
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 app.use(cors());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  })
 
 connect();
 // app.listen(port, () => {
